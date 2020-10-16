f9fdbaf95010b90c807698747bee6bd5ccae6db6
wise-king-sullyman <a.m.sullivan.github@gmail.com>
2020-10-15 23:01:30 -0400

    It is pretty ugly and I don't feel good about it, but I have an algorithm for finding possible moves

--- knights_travails.rb
+++ knights_travails.rb
@@ -31,23 +31,62 @@ end
 class Knight
   attr_accessor :board
 
-  def initialize
+  def initialize(location, destination)
     @board = Board.new
+    @location = location
+    @destination = destination
+    move(@location.first, @location.last)
+    draw_destination
   end
 
   def move(row, column)
     @board.update(row, column, 'X ')
   end
 
-  def destination(row, column)
-    @board.update(row, column, 'O ')
+  def draw_destination
+    @board.update(@destination.first, @destination.last, 'O ')
+  end
+
+  def possible_moves(location = @location)
+    moves = []
+    x = -1
+    8.times do
+      x += 1
+      y = -1
+      8.times do
+        y += 1
+        pos_move = Moves.new(x, y)
+        moves.push(pos_move) if pos_move.legal(location)
+      end
+    end
+    moves.each { |pos_move| move(pos_move.row, pos_move.column)}
+  end
+end
+
+class Moves
+  include Comparable
+  attr_accessor :row, :column
+  def initialize(row, column)
+    @row = row
+    @column = column
+    possible_moves = []
+  end
+
+  def legal(location)
+    return false unless @row.between?(0, 7) && @column.between?(0, 7)
+
+    return false if @row == location.first && @column == location.last
+    
+    row_diff = (location.first - @row).abs
+    col_diff = (location.last - @column).abs
+    true if row_diff == 2 && col_diff == 1 || row_diff == 1 && col_diff == 2
   end
 end
 
 def knight_moves(from, to)
 end
 
-knight = Knight.new
-knight.move(1, 5)
-knight.destination(6, 2)
+knight = Knight.new([1, 5], [6, 2])
+#puts knight.board
+knight.possible_moves
 puts knight.board
